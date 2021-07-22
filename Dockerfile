8d8c0527cea0f20a236e1c9760c3fceec2d82136
rtrier <ralf.trier@gdi-service.de>
2021-07-22 13:50:29 +0200

    multistage build

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,27 @@
+FROM maven:3.8-jdk-11-openj9 as maven
+LABEL maintainer="Ralf Trier GDI-Service"
+LABEL version="0.2.0"
+
+WORKDIR /app
+COPY  src /app/src
+RUN ls -al /app/src/*
+COPY  pom.xml /app/
+RUN mvn package
+RUN ls -al /app/target/*
+RUN ls -al /app/target/de.gdiservice.cmdserver-*.jar
+
 FROM osgeo/gdal:ubuntu-full-3.2.2
 
-LABEL maintainer="Ralf Trier GDI-Service"
-LABEL version="0.1.0"
+COPY --from=maven /app/target/de.gdiservice.cmdserver-*.jar /cmdserver/lib/main.jar
+COPY --from=maven /app/target/alternateLocation /cmdserver/lib/
+COPY cmd.properties /cmdserver/classes/
 
-ADD /lib/* /cmdserver/lib/
-ADD /classes/* /cmdserver/classes/
+RUN ls -al /cmdserver/*
+RUN ls -al /cmdserver/lib/main.jar
+RUN unzip -l /cmdserver/lib/main.jar
+
 WORKDIR /cmdserver
 
 EXPOSE 8080
 
-ENTRYPOINT [ "java", "-Djava.awt.headless=true", "-cp", "/cmdserver/classes/:/cmdserver/lib/de.gdiservice.cmdserver-0.0.1.jar", "de.gdiservice.cmdserver.CmdServer", "server"]
+ENTRYPOINT [ "java", "-Djava.awt.headless=true", "-cp", "/cmdserver/classes/:/cmdserver/lib/main.jar", "de.gdiservice.cmdserver.CmdServer", "server"]
--- src/main/java/de/gdiservice/cmdserver/Tokenizer.java
+++ src/main/java/de/gdiservice/cmdserver/Tokenizer.java
@@ -1,66 +1,58 @@
 package de.gdiservice.cmdserver;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 
 public class Tokenizer {
-    
-
-    
-
-    
     
     static String[] getTokens(String s) {
         
         System.out.println(s);
         List<String> tokens = new ArrayList<>();
         
-          
         Character esChar = null;
         
-        int tokenStart = 0;
-        int idx = 0;
+        StringBuilder sb = new StringBuilder();
         
-        for (int i=0, count=s.length(); i<count; i++) {
-
-            char ch = s.charAt(i);            
-            if (ch=='"' || ch=='\'') {
-                if (esChar ==null) {
-                    esChar = ch;
-                    idx++;
-                } else {
-                    if (esChar.equals(ch)) {
-                        esChar=null;
-                    }
-                    idx++;
-                }
-            } else if (ch == ' ') {
-                if (esChar == null) {
-                    String token = s.substring(tokenStart, idx);
-                    // System.out.println(token);
-                    final String t = token.replace("\"", "");
-                    tokens.add(t);
-                    tokenStart = idx+1;
-                    idx++;
-                }
-                else {                    
-                    idx++;
-                }
+        for (int i=0, count=s.length(); i<count; i++) {            
+            char ch = s.charAt(i);
+            if (ch == '\\' && i<count-1 && (s.charAt(i+1)=='"' || s.charAt(i+1)=='\\')) {
+                sb.append(s.charAt(i+1));
+                i++;
             } else {
-                idx++;
+                if (ch=='"' || ch=='\'') {
+                    if (esChar == null) {
+                        esChar = ch;
+                    } else {
+                        if (esChar.equals(ch)) {
+                            esChar=null;
+                        } else {
+                          sb.append(ch);                        
+                        }
+                    }
+                } else if (ch == ' ') {
+                    if (esChar == null) {
+                        tokens.add(sb.toString());
+                        sb.setLength(0);
+                    }
+                    else {                    
+                        sb.append(ch);
+                    }
+                } else {
+                    sb.append(ch);
+                }
             }
         }
         
-        tokens.add(s.substring(tokenStart, idx));
+        tokens.add(sb.toString());        
         return tokens.toArray(new String[tokens.size()]);
         
     }
     
     
     public static void main(String[] args) {
-        String s = "-f \"PostgreSQL\" PG:\"host='pgsql' port='5432' dbname='kvwmapsp' user='kvwmap' ACTIVE_SCHEMA=testschema_ralf \" GMLAS:/var/www/tmp/temp.gml_2.gml -oo REMOVE_UNUSED_LAYERS=YES -oo XSD=/var/www/html/modell/xsd/5.1/XPlanung-Operationen.xsd -lco SCHEMA=testschema_ralf";
+        // String s = "-f \"PostgreSQL\" PG:\"host='pgsql' port='5432' dbname='kvwmapsp' user='kvwmap' ACTIVE_SCHEMA=testschema_ralf \" GMLAS:/var/www/tmp/temp.gml_2.gml -oo REMOVE_UNUSED_LAYERS=YES -oo XSD=/var/www/html/modell/xsd/5.1/XPlanung-Operationen.xsd -lco SCHEMA=testschema_ralf";
+        String s = "-lco FID=gid -f PostgreSQL -lco GEOMETRY_NAME=the_geom -lco precision=NO -nlt PROMOTE_TO_MULTI -nln afrei_1polygon908580 -a_srs EPSG:2398 -append PG:\"host='pgsql' port='5432' dbname='kvwmapsp' user='k\\\\\\\"vwmap' password='-1$ß~bN\\\"6&ZWPe}B_YpA+l' active_schema=custom_shapes\" \"/var/www/data/upload/11/Frei 1Polygon.dbf\"";
         // String s = "-f \"PostgreSQL\" PG:\"host='pgsql'";
         String[] tokens = getTokens(s);
         for (int i=0; i<tokens.length; i++) {
