56cc3d372873c178ca6ba9cf975b247f285dd258
rtrier <ralf.trier@gdi-service.de>
2021-07-22 13:45:07 +0200

    error message specified

--- src/main/java/de/gdiservice/cmdserver/CmdServer.java
+++ src/main/java/de/gdiservice/cmdserver/CmdServer.java
@@ -17,156 +17,169 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 class CmdServer {
-	
-	static class Result {
-		@JsonProperty
-		private int exitCode;
-		@JsonProperty
-		private String stdout;
-		@JsonProperty
-		private String stderr;
 
-		Result(int exitCode, String out, String err) {
-			this.exitCode = exitCode;
-			this.stdout = out;
-			this.stderr = err;
-		}
+    static class Result {
+        @JsonProperty
+        private String err;
+        @JsonProperty
+        private int exitCode;
+        @JsonProperty
+        private String stdout;
+        @JsonProperty
+        private String stderr;
 
-		@Override
-		public String toString() {
-			return "Result [exitCode=" + exitCode + ", out= \"" + stdout + "\", err=\"" + stderr + "\"]";
-		}
-		
-	}
-	
-	class Consumer extends Thread {
-		private BufferedReader in;
-		private String result;
+        Result(int exitCode, String stdout, String stderr) {
+            this.exitCode = exitCode;
+            this.stdout = stdout;
+            this.stderr = stderr;
+        }
 
-		Consumer(InputStream in) {
-			// "ISO-8859-1"
-			// System.out.println(Charset.defaultCharset());
-			this.in = new BufferedReader( new InputStreamReader(in, Charset.defaultCharset()));
-		}
-		public void run() {
-			StringBuilder sb =  new StringBuilder();
-			String s;
-			try {
-				while ((s=in.readLine())!=null) {
-					sb.append(s).append("\r\n");
-				}
-			}
-			catch (IOException ex) {
-				ex.printStackTrace();
-			}
-			this.result = sb.toString();
-			System.out.println(result);
-		}
-		String getResult( ) {
-			return result;
-		}
-	}
+        Result(String err) {
+            this.err = err;
+        }
 
-	private final Properties properties;
+        @Override
+        public String toString() {
+            return "Result [exitCode=" + exitCode + ", stdout= \"" + stdout + "\", stderr=\"" + stderr + "\", err=\"" + err + "\"]";
+        }
+
+    }
+
+    class Consumer extends Thread {
+        private BufferedReader in;
+        private String result;
+
+        Consumer(InputStream in) {
+            // "ISO-8859-1"
+            // System.out.println(Charset.defaultCharset());
+            this.in = new BufferedReader( new InputStreamReader(in, Charset.defaultCharset()));
+        }
+        public void run() {
+            StringBuilder sb =  new StringBuilder();
+            String s;
+            try {
+                while ((s=in.readLine())!=null) {
+                    sb.append(s).append("\r\n");
+                }
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            this.result = sb.toString();
+            System.out.println(result);
+        }
+        String getResult( ) {
+            return result;
+        }
+    }
+
+    private final Properties properties;
     private HttpServer server;
-	
-	CmdServer() throws IOException {
-		Properties prop = new Properties();
-		prop.load(this.getClass().getClassLoader().getResourceAsStream("cmd.properties"));
-		this.properties = prop;
-	}
-	
-	private String[] concat(String s, String[] params) {
-		if (params!=null) {
-			String[] res = new String[params.length+1];
-			res[0] = s;
-			System.arraycopy(params, 0, res, 1, params.length);
-			return res;
-		}
-		return new String[] {s};
-	}
-	
-	private Result runProzess(String cmd, String[] params) throws IOException {
-		String[] cmds = concat(cmd, params);
-		System.err.println("cmds="+Arrays.toString(cmds));
-		ProcessBuilder pb = new ProcessBuilder(cmds);
-		
-		Process process = pb.start();
-		Consumer stdout = new Consumer(process.getInputStream());
-		stdout.start();
-		Consumer stderr = new Consumer(process.getErrorStream());
-		stderr.start();
-		try {
-			int code = process.waitFor();
-			return new Result( code, stdout.getResult(), stderr.getResult());
-		} 
-		catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	
-	
-	private void start() {
 
-		HttpServer server = HttpServer.createSimpleServer();
-		server.getServerConfiguration().setDefaultQueryEncoding(Charset.forName("utf8"));
-		server.getServerConfiguration().addHttpHandler(
-		    new HttpHandler() {
-		        public void service(Request request, Response response) throws Exception {
-		        	System.err.println("getDecodedRequestURI:\""+request.getDecodedRequestURI()+"\"");
-		        	System.err.println("pathInfo:\""+request.getPathInfo()+"\"");
-		        	System.err.println("tool:\""+request.getParameter("tool")+"\"");
-		        	System.err.println("param:\""+request.getParameter("param")+"\"");
-		        	
-		        	String tool = request.getParameter("tool");
-		        	if (tool!=null) {
-			        	String param = request.getParameter("param");
-			        	String cmd = properties.getProperty(request.getParameter("tool"));
-			        	if (cmd!=null) {
-			        		String[] params = (param!=null) ? Tokenizer.getTokens(param) : null;
-			        		if (params!=null) {
-			        		    for (int i=0; i<params.length; i++) {
-			        		        System.out.println(""+i+"\""+params[i]+"\"");
-			        		    }
-			        		}
-				        	Result result = runProzess(cmd, params);		        	
-				            response.setContentType("application/json");
-				            ObjectMapper objectMapper = new ObjectMapper();
-				            objectMapper.writer().writeValue(response.getOutputStream(), result);
-			        	}
-		        	}
-		        }
-		    });
-		try {
-		    server.start();
-		    this.server = server;		    
-		    Runtime.getRuntime().addShutdownHook(new ShutdownThread());
-		    Thread.currentThread().join();
-//		    System.out.println("Press any key to stop the server...");
-//		    System.in.read();
-		} catch (Exception e) {
-		    System.err.println(e);
-		
-		}		
-	}
-	
-	class ShutdownThread extends Thread {
-	    public void run() {
-	        CmdServer.this.server.shutdown();
-	    }
-	}
+    CmdServer() throws IOException {
+        Properties prop = new Properties();
+        prop.load(this.getClass().getClassLoader().getResourceAsStream("cmd.properties"));
+        this.properties = prop;
+    }
 
-	public static void main(String[] args) {
-		try {
-			CmdServer server = new CmdServer();
-			server.start();
-		} 
-		catch (Throwable ex) {
-			ex.printStackTrace();
-		}
-	}
-	
+    private String[] concat(String s, String[] params) {
+        if (params!=null) {
+            String[] res = new String[params.length+1];
+            res[0] = s;
+            System.arraycopy(params, 0, res, 1, params.length);
+            return res;
+        }
+        return new String[] {s};
+    }
+
+    private Result runProzess(String cmd, String[] params) throws IOException {
+        String[] cmds = concat(cmd, params);
+        System.err.println("cmds="+Arrays.toString(cmds));
+        ProcessBuilder pb = new ProcessBuilder(cmds);
+
+        Process process = pb.start();
+        Consumer stdout = new Consumer(process.getInputStream());
+        stdout.start();
+        Consumer stderr = new Consumer(process.getErrorStream());
+        stderr.start();
+        try {
+            int code = process.waitFor();
+            return new Result( code, stdout.getResult(), stderr.getResult());
+        } 
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+    private void start() {
+
+        HttpServer server = HttpServer.createSimpleServer();
+        server.getServerConfiguration().setDefaultQueryEncoding(Charset.forName("utf8"));
+        server.getServerConfiguration().addHttpHandler(
+                new HttpHandler() {
+                    public void service(Request request, Response response) throws Exception {
+                        System.err.println("getDecodedRequestURI:\""+request.getDecodedRequestURI()+"\"");
+                        System.err.println("pathInfo:\""+request.getPathInfo()+"\"");
+                        System.err.println("tool:\""+request.getParameter("tool")+"\"");
+                        System.err.println("param:\""+request.getParameter("param")+"\"");
+
+                        String tool = request.getParameter("tool");
+                        Result result;
+                        if (tool!=null) {
+                            String param = request.getParameter("param");
+                            String cmd = properties.getProperty(request.getParameter("tool"));
+                            if (cmd!=null) {
+                                String[] params = (param!=null) ? Tokenizer.getTokens(param) : null;
+                                if (params!=null) {
+                                    for (int i=0; i<params.length; i++) {
+                                        System.out.println(""+i+"\""+params[i]+"\"");
+                                    }
+                                }
+                                result = runProzess(cmd, params);		        	
+
+                            } else {
+                                result = new Result("tool not supported");
+                            }
+                            
+                        } else {
+                            result = new Result("parameter is missing");
+                        }
+                        response.setContentType("application/json");
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        objectMapper.writer().writeValue(response.getOutputStream(), result);
+                    } 
+                });
+        try {
+            server.start();
+            this.server = server;		    
+            Runtime.getRuntime().addShutdownHook(new ShutdownThread());
+            Thread.currentThread().join();
+            //		    System.out.println("Press any key to stop the server...");
+            //		    System.in.read();
+        } catch (Exception e) {
+            System.err.println(e);
+
+        }		
+    }
+
+    class ShutdownThread extends Thread {
+        public void run() {
+            CmdServer.this.server.shutdown();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            CmdServer server = new CmdServer();
+            server.start();
+        } 
+        catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+    }
+
 
 }
